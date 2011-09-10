829e75ff6c17a408bca0c51aec94dc95282ebec7
Caike Souza <carloshrsouza@gmail.com>
2011-09-10 16:56:39 -0400

    dojo work

--- test/ClojureTemplate/test/core.clj
+++ test/ClojureTemplate/test/core.clj
@@ -1,27 +1,21 @@
 (ns ClojureTemplate.test.core
-  (:use [ClojureTemplate.core])
   (:use [clojure.test]))
 
-(deftest sum-works
-  (is 4 (add 2 2)))
+(defn next-multiple [cursor multiplier]
+  (+ cursor multiplier)
+  )
+  
+(defn gen-multiples[limit, n]
+  (let return (gen-next-mult))
+  (if (< limit (gen-next-multiple 0 3) ))
+)
 
-(deftest sub-works
-  (is 3 (sub 7 4)))
+(deftest gen-next-multiple
+  (is 6 (next-multiple 3 3))
+  (is 15 (next-multiple 12 3))
+  )
 
-(deftest sum-multiple-works
-  (is 9 (add 3 2 4)))
-
-(deftest four-is-not-prime
-  (not (prime? 4)))
-
-(deftest seven-is-prime
-  (is (prime? 7)))
-
-(deftest thirteen-is-prime
-  (is (prime? 13)))
-
-(deftest forteen-is-not-prime
-  (not (prime? 13)))
-
-(deftest seventeen-is-prime
-  (is (prime? 17)))
+(deftest gen-multiples-test1
+  (is (= (list 3 5 6 9) (gen-multiples 10)) )
+  (is (= (list 3 5 6 9 10 12) (gen-multiples 15) ))
+)
